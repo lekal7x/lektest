d2a215cd1a6201647ad06c010b1224771b8183b5
lekal7x <iek2000@hotmail.com>
2024-08-09 16:49:59 +0700

    Update lek.sh

--- lek.sh
+++ lek.sh
@@ -3,15 +3,17 @@
 # 1. ดาวน์โหลดไฟล์ติดตั้ง Falcon Sensor
 wget https://dtdreport.inet.co.th/index.php/s/9DrKsMkXCTpbfmy/download/falcon-sensor_Ubuntu_7.03.0-15805_amd64.deb
 
-# 2. แสดงรายการไฟล์ในไดเรกทอรีปัจจุบัน
+# 2. รอให้การดาวน์โหลดเสร็จสมบูรณ์
+wait
+
+# 3. แสดงรายการไฟล์ในไดเรกทอรีปัจจุบัน
 ls
 
-# 3. เปลี่ยนสิทธิ์ของไฟล์ติดตั้งเป็น 777
-chmod 777 falcon-sensor_7.03.0-15805_amd64.deb
+# 4. เปลี่ยนสิทธิ์ของไฟล์ติดตั้งเป็น 777
+chmod 777 falcon-sensor_Ubuntu_7.03.0-15805_amd64.deb
 
-# 4. แสดงรายการไฟล์ในไดเรกทอรีปัจจุบันอีกครั้ง
+# 5. แสดงรายการไฟล์ในไดเรกทอรีปัจจุบันอีกครั้ง
 ls
 
-# 5. ติดตั้งแพ็กเกจ Falcon Sensor
-dpkg -i falcon-sensor_7.03.0-15805_amd64.deb
-
+# 6. ติดตั้งแพ็กเกจ Falcon Sensor
+dpkg -i falcon-sensor_Ubuntu_7.03.0-15805_amd64.deb
