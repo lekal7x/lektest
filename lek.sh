124a301ea9bd89af609b0fb8a6ff4ba9f0303482
lekal7x <iek2000@hotmail.com>
2024-08-09 16:44:30 +0700

    Update lek.sh

--- lek.sh
+++ lek.sh
@@ -15,17 +15,3 @@ ls
 # 5. ติดตั้งแพ็กเกจ Falcon Sensor
 dpkg -i falcon-sensor_7.16.0-16903_amd64.deb
 
-# 6. ตั้งค่า CID (Customer ID) สำหรับ Falcon Sensor
-/opt/CrowdStrike/falconctl -s --cid=00F79DE8BE94463D8482591A1A4B5AF2-85
-
-# 7. เปิดใช้งานการเริ่มต้นอัตโนมัติของ Falcon Sensor
-systemctl enable falcon-sensor
-
-# 8. เริ่มต้นบริการ Falcon Sensor
-systemctl start falcon-sensor
-
-# 9. ตรวจสอบสถานะของบริการ Falcon Sensor
-systemctl status falcon-sensor
-
-# 10. อีกวิธีในการตรวจสอบสถานะของ Falcon Sensor
-service falcon-sensor status
